7dcd94eaee537b97c4f7fdce9aa7b9fb55a0e2f5
bogdan2503 <korovjakbogdan@gmail.com>
2022-02-21 16:27:45 +0200

    done

--- hw1/js/hw1.js
+++ hw1/js/hw1.js
@@ -1,1 +1,49 @@
-alert ("homework1");
+// alert ("Korovjak");
+
+// task 3;
+// let x = 10
+// let y = "bogdan";
+// alert (x);
+// alert (y);
+// y = x;
+// alert (x);
+// alert (y);
+
+// task 4
+// let x = {
+//     firstName: "Bogdan",
+//     age: 31,
+//     isAdult: true,
+//     isMaried: undefined,
+//     childrens: null,
+// };
+// console.log (x);
+
+// task 5
+// let isAdult = confirm ("Are you an adult?");
+// console.log (isAdult)
+
+// task 6
+// let firstName = "Bogdan";
+// let lastName = "Korovjak";
+// let studyGroup = "Lv-678.JSCore";
+// let isMaried = false;
+// let yearOfBirth = 1990;
+// console.log (typeof firstName, typeof lastName, typeof studyGroup, typeof isMaried);
+// console.log (yearOfBirth, isMaried, firstName, lastName, studyGroup);
+
+// let x = null;
+// let y;
+// console.log (x, y)
+
+// task 7
+// let login = prompt ("enter your login");
+// let email = prompt ("enter you email");
+// let password = prompt ("enter your password");
+// alert ("Dear" + " " + login + ", " + "your email is" + " " + email + ", " + "your passwor is " + password);
+
+// task 8
+let hours = (60*60);
+let day = (24*60*60);
+let month = (30*24*60*60);
+alert ("Seconds in hour " + hours + ", seconds in day " + day + ", seconds in month " + month +".");
