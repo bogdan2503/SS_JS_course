365b0fecfe83361e0a8d0e98a15d130b7370d114
bogdan2503 <korovjakbogdan@gmail.com>
2022-02-23 02:30:36 +0200

    hw2

---
+++ hw2/js/hw2.js
@@ -0,0 +1,33 @@
+// task 1
+// let x = 1;
+// let y = 2;
+
+// let res1 = String(x)+String(y);
+// console.log (res1);
+// console.log (typeof res1);
+
+// let res2 = Boolean(x)+String(y);
+// console.log (res2);
+// console.log (typeof res2)
+
+// let res3= y>x;
+// console.log (res3);
+// console.log (typeof res3)
+
+// let res4 = parseInt (x>y);
+// console.log (res4)
+// console.log (typeof res4);
+
+// task 2
+
+// let k = prompt ("enter number");
+// if (k%2===0 && k>0){
+//     console.log ("ваше число парне і додатне")  
+// }  else {
+//     console.log ("ваше число не парне або не додатне")
+// } 
+
+// let l = prompt ("enter number");
+// if (l%7===0){
+//     console.log ("ваше число кратне 7")
+// }
