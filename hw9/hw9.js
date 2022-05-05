3dff63863425f869e490b10730c02ce853981e59
bogdan2503 <korovjakbogdan@gmail.com>
2022-05-06 01:08:50 +0300

    hw9

---
+++ hw9/hw9.js
@@ -0,0 +1,25 @@
+//////////////////////////  task 1 //////////////////////////
+
+// $('a[href^="https://"]').attr ("target","_blank")
+
+//////////////////////////  task 2 //////////////////////////
+
+// $ ("h2.head").css ("background-color", "green")
+// $ ("h2.head").find (".inner").css ("font-size", "35px")
+
+//////////////////////////  task 3 //////////////////////////
+
+// $ ("h3+div").each(function(){
+//     let x = $(this)
+//     x.prev().before(x)
+// })
+
+//////////////////////////  task 4 //////////////////////////
+
+// let ch = $ (':checkbox')
+// ch.on('click', function () {
+//     if ($(':checkbox:checked').length == 3) 
+//     {
+//         ch.attr ('disable',"disable")
+//     }
+//     })
